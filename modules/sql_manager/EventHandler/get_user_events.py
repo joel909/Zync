66e759f6b3f552fceb8c27023b116da13c615b85
Joel Joby <joeljobyp@gmail.com>
2025-10-23 09:54:58 +0530

    FIXED frontend issue

--- modules/sql_manager/EventHandler/get_user_events.py
+++ modules/sql_manager/EventHandler/get_user_events.py
@@ -1,5 +1,5 @@
 def get_user_events(self,auth_key):
             query = f'select * from events where auth_key = "{auth_key}";'
-
-
-    
+            self.cursor.execute(query)
+            result = self.cursor.fetchall()
+            return result
