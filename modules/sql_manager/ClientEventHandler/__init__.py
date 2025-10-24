ea257ed88f54cd2b14cc885b7a8c7c3ae1224827
Joel Joby <joeljobyp@gmail.com>
2025-10-24 10:26:20 +0530

    update

--- modules/sql_manager/ClientEventHandler/__init__.py
+++ modules/sql_manager/ClientEventHandler/__init__.py
@@ -1,3 +1,4 @@
+from .register_for_event import register_for_event
 class ClientEventHandler():
     def __enter__(self):
         # setup code
@@ -6,6 +7,7 @@ class ClientEventHandler():
         self.connection = connection
         self.cursor = cursor
     def register_for_event(self,event_id,name,school,dob,grade,contact):
+        return register_for_event(self,event_id,name,school,dob,grade,contact)
         # Logic to register a user for an event
         pass
     def __exit__(self, exc_type, exc_val, exc_tb):
